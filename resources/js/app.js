bd372a1787994f900cef7a0d8f9da3fe5968b403
wufly <fei.wu@waiwaimall.com>
2018-12-13 13:30:03 +0800

    构建应用

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,15 +22,6 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+import Vue from 'vue';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
-});
+new Vue().$mount('#app');
