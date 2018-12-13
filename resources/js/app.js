914b4a46b5af854e57de2a11e0ab1eea042b70a0
wufly <fei.wu@waiwaimall.com>
2018-12-13 17:56:16 +0800

    创建了一个 Vuex 存储器并为咖啡店配置了一个 Vuex 模块

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,8 +24,10 @@ if (token) {
 
 import Vue from 'vue';
 import router from './routes.js'
+import store from './store.js'
 
 const app = new Vue({
     el: '#app',
     router,
+    store
 });
---
+++ resources/js/store.js
@@ -0,0 +1,28 @@
+/**
+ * Adds the promise polyfill for IE 11
+ */
+require('es6-promise').polyfill();
+/**
+ * Import Vue and Vuex
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/**
+ * Initializes Vuex on Vue.
+ */
+Vue.use( Vuex )
+
+/**
+ * Imports all of the modules used in the application to build the data store.
+ */
+import { cafes } from './modules/cafes.js'
+
+/**
+ * Export the data store.
+ */
+export default new Vuex.Store({
+    modules: {
+        cafes
+    }
+});
